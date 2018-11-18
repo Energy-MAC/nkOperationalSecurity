88cee517eb50e2b26f3e11d7ac1b2e7e7c0d9f5d
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-18 14:49:42 -0800

    Fix primal

--- src/Primal_problem.jl
+++ src/Primal_problem.jl
@@ -23,11 +23,38 @@ function generators_limits(m, P_th, devices)
         
 end
     
-function primal_problem(generators, buses, lines, loads)    
-    #Instantiate Model
-    ED = Model(with_optimizer(Ipopt.Optimizer))
     
-    set_gens = [g.name for g in  generators14 if g.available]
-    set_loads = [ld.name for ld in loads14 if ld.available] 
-    set_lines = [ln.name for ln in branches14 if ln.available]
-    set_buses = [b.name for b in nodes14]
+    
+function primal_problem(generators, buses, branches, loads)    
+    #Instantiate Model
+    PM = Model(with_optimizer(Ipopt.Optimizer))
+    
+    #make sets
+    set_gens = [g.name for g in  generators if g.available]
+    set_loads = [ld.name for ld in loads if ld.available] 
+    set_lines = [ln.name for ln in branches if ln.available]
+    set_buses = [b.name for b in buses]
+    
+    #generate variables     
+    @variable(PM, P_th[set_gens], lower_bound = 0)
+    @variable(PM, D[set_loads], lower_bound =  0) 
+        
+    for (ix,d) in enumerate(PM[:D])
+        JuMP.set_start_value(d,loads14[ix].maxactivepower)
+    end
+
+    @variable(PM, fl[set_lines]) 
+
+    @variable(PM, z[set_lines] == 0.0) # add the Bin tag Later in order to make this code run with Ipopt
+
+    @variable(PM, θ[set_buses]);  
+        
+    for name in θ.axes[1][2:end]
+        JuMP.set_lower_bound(θ[name],-1.57)
+        JuMP.set_upper_bound(θ[name],1.57)
+    end
+        
+    JuMP.fix(θ["Bus 1"],0.0)     
+    
+    generators_limits(PM, P_th, generators)        
+end 
--- src/nKOS.jl
+++ src/nKOS.jl
@@ -4,10 +4,10 @@ using SCS
 using Ipopt
 using GLPK
 
-const JumpVariable = JuMP.JuMPArray{JuMP.VariableRef,2,Tuple{Array{String,1},UnitRange{Int64}}}
-const JumpExpressionMatrix = Matrix{<:JuMP.GenericAffExpr}
-const JumpAffineExpressionArray = Array{JuMP.GenericAffExpr{Float64,JuMP.VariableRef},2}
+#const JumpVariable = JuMP.JuMPArray{JuMP.VariableRef,2,Tuple{Array{String,1},UnitRange{Int64}}}
+#const JumpExpressionMatrix = Matrix{<:JuMP.GenericAffExpr}
+#const JumpAffineExpressionArray = Array{JuMP.GenericAffExpr{Float64,JuMP.VariableRef},2}
 
 include("simple_lp.jl")
 include("../data/data_14bus_pu.jl")
-include("Primal_problem")
+include("Primal_problem.jl")
