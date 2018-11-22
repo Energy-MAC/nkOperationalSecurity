dc9569f6d364f9254a933cbdeb50c774fc074037
Ciaran <ciaran_r@berkeley.edu>
2018-11-21 23:56:45 -0800

    changed naming convention for consistency

--- src/generate_118_bus.jl
+++ src/generate_118_bus.jl
@@ -1,4 +1,4 @@
 file = expanduser("~")*"/Documents/nkOperationalSecurity/data/nesta_case118_ieee_nk.m"
 data = PowerSystems.parse_matpower(file)
 ps_dict = PowerSystems.pm2ps_dict(data)
-buses,generators,storage, branches, loads, loadZones, shunts = PowerSystems.ps_dict2ps_struct(ps_dict)
+nodes118,generators118,storage118, branches118, loads118, loadZones118, shunts118 = PowerSystems.ps_dict2ps_struct(ps_dict)
