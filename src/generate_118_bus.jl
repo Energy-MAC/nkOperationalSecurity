ca091f7824757302354a9e1d3c2806fa41ebb732
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-23 23:51:51 -0600

    Modify path

--- src/generate_118_bus.jl
+++ src/generate_118_bus.jl
@@ -1,4 +1,4 @@
-file = "../data/nesta_case118_ieee_nk.m"
+file = "data/nesta_case118_ieee_nk.m"
 data = PowerSystems.parse_matpower(file)
 ps_dict = PowerSystems.pm2ps_dict(data)
 nodes118,generators118,storage118, branches118, loads118, loadZones118, shunts118 = PowerSystems.ps_dict2ps_struct(ps_dict)
