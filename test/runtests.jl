055476b8c0258833f0155c5bbae22e0900808750
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-26 16:58:58 -0500

    fix testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,14 @@
 using Test
 @test include("../src/nKOS.jl");
-@test try simple_lp() end
+
+#=
+@testset "Run basics" begin
+    @test try   BM = bigM_version(generators14, nodes14, branches14, loads14, 5000); JuMP.optimize!(BM); true finally end
+    @test try  NL = NL_version(generators14, nodes14, branches14, loads14); JuMP.optimize!(NL); true finally end
+end
+=#
+
+@testset "Run NK Versions" begin
+    @test try   BM = bigM_version(generators14, nodes14, branches14, loads14, 5000); JuMP.optimize!(BM); true finally end
+    @test try  NL = NL_version(generators14, nodes14, branches14, loads14); JuMP.optimize!(NL); true finally end
+end
