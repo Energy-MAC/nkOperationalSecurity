273db496f61739b69eadef79db856de68cdd3c7a
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-27 11:43:55 -0800

    add load percentage syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,6 @@ end
 
 include("../src/nKOS.jl");
 @testset "Run N-K Versions" begin
-    @test try   BM = bigM_version(generators14, nodes14, branches14, loads14, 5000); JuMP.optimize!(BM); true finally end
-    @test try  NL = NL_version(generators14, nodes14, branches14, loads14); JuMP.optimize!(NL); true finally end
+    @test try   BM = bigM_version(generators14, nodes14, branches14, loads14, 5000, 0.7); JuMP.optimize!(BM); true finally end
+    @test try  NL = NL_version(generators14, nodes14, branches14, loads14, 0.7); JuMP.optimize!(NL); true finally end
 end
