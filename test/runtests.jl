9b436c3ebd532a32a5114baee6cd8adbffe38a24
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-27 11:28:05 -0800

    minor typo fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ end
 =#
 
 include("../src/nKOS.jl");
-@testset "Run NK Versions" begin
+@testset "Run N-K Versions" begin
     @test try   BM = bigM_version(generators14, nodes14, branches14, loads14, 5000); JuMP.optimize!(BM); true finally end
     @test try  NL = NL_version(generators14, nodes14, branches14, loads14); JuMP.optimize!(NL); true finally end
 end
