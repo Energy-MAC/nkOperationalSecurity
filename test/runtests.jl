63b14678635ce0e4bfc8895e468e2bf283219539
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-27 19:25:04 -0800

    Improve Testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,31 @@ using Test
 
 include("../src/nKOS.jl");
 
-@testset "Run basics" begin
+@testset "Run n-1 faults, check for Duality Conditions removing line" begin
     for k in 1:length(branches14)
     @test try        
         s = setdiff!(collect(1:length(branches14)), [k])
         branches_k = branches14[s]
         PP = primal_problem(generators14, nodes14, branches_k, loads14, with_optimizer(GLPK.Optimizer))
         DP = dual_problem(generators14, nodes14, branches_k, loads14, with_optimizer(GLPK.Optimizer))
+        JuMP.optimize!(PP)
+        JuMP.optimize!(DP)
+        println(isapprox(JuMP.objective_value(PP), JuMP.objective_value(DP), atol=1e-1)) 
+        isapprox(JuMP.objective_value(PP), JuMP.objective_value(DP), atol=1e-1)
+        finally
+        end
+    end
+end     
+        
+@testset "Run n-1 faults, check for Duality Conditions fixing z" begin
+    for k in 1:length(branches14)
+    @test try        
+        z = zeros(length(branches14))
+        s = setdiff!(collect(1:length(branches14)), [k])
+        branches_k = branches14[s]
+        z[k] = 1.0    
+        PP = primal_problem(generators14, nodes14, branches_k, loads14, with_optimizer(GLPK.Optimizer))
+        DP = dual_problem_test(generators14, nodes14, branches_k, loads14, z, with_optimizer(GLPK.Optimizer))
         JuMP.optimize!(PP)
         JuMP.optimize!(DP)
         println(isapprox(JuMP.objective_value(PP), JuMP.objective_value(DP), atol=1e-1)) 
