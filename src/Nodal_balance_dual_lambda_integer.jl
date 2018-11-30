7fee88f0536c87e3aa6e02d34dd289e5423b9f89
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-30 14:04:51 -0800

    update bounds

--- src/Nodal_balance_dual_lambda_integer.jl
+++ src/Nodal_balance_dual_lambda_integer.jl
@@ -8,8 +8,8 @@ end
 
 
 function relaxation_bilinear_i(m, x, z, w)
-    x_ub = 5000
-    x_lb = -5000
+    x_ub = 80
+    x_lb = -80
 
     L = @variable(m, [1:2],upper_bound = 1.0, lower_bound = 0.0)
 
