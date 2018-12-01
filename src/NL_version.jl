a5c5083b557a84a7e2e3b3e44ba61f72bdf73d98
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-30 21:15:14 -0800

    enable warm starting for NL Models

--- src/NL_version.jl
+++ src/NL_version.jl
@@ -2,10 +2,27 @@ function NL_version(generators, nodes, branches, loads, min_load_percent, optimi
                 
     DM = Model(optimizer)
             
-    dual_variables(DM, generators, nodes, branches, loads)
+       total_load = 0.0
+    for l in loads
+        total_load=total_load+l.maxactivepower
+    end
+    set_gens = [g.name for g in generators if g.available];
+    set_loads = [l.name for l in loads if l.available];
+    set_buses = [b.name for b in nodes];
+    set_branches = [ell.name for ell in branches];
+                
+    @variable(DM, μ_plus[set_gens], lower_bound = 0.0,start=0.0);
+    @variable(DM, β_plus[set_loads], lower_bound = 0.0,start=0.0);
+    @variable(DM, α_plus[set_branches], lower_bound = 0.0,start=0.0);
+    @variable(DM, α_minus[set_branches], lower_bound = 0.0,start=0.0);
+    @variable(DM, ν_plus[set_buses], lower_bound = 0.0,start=0.0);
+    @variable(DM, ν_minus[set_buses], lower_bound = 0.0,start=0.0);
+    @variable(DM, λ[set_buses], start=0.0);
+    @variable(DM, η[set_branches], lower_bound=-2*total_load, upper_bound=2*total_load, start=0.5);
+    @variable(DM, ζ, start=0.0);
     
     set_branches = [ell.name for ell in branches];
-    @variable(DM, z[set_branches],lower_bound = 0.0,upper_bound = 1.0);
+    @variable(DM, z[set_branches],lower_bound = 0.0,upper_bound = 1.0, start = 0.5);
     
     dual_gens(DM, nodes, generators)
     dual_loads(DM,  nodes, loads)                
